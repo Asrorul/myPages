d879100a6e9e43a6114ac1882ae3cd93855ba54e
Asrorul <54773479+Asrorul@users.noreply.github.com>
2025-03-13 06:58:34 +0700

    update workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myPages/',
   test: {
     globals: true,
     environment: 'jsdom',
